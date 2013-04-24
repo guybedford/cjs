5e8bf30441e9caac09e8b4199095381ebc2964be
Guy Bedford <guybedford@gmail.com>
2013-04-23 20:12:48 -0700

    Update cjs.js

--- cjs.js
+++ cjs.js
@@ -1,9 +1,13 @@
 define(['amd-loader'], function(amdLoader) {
   var cjsRequireRegExp = /\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g;
   return amdLoader('cjs', 'js', function(name, source, req, callback, errback, config) {
-    // replace internal requires with common js requires themselves
+    // replace internal relative requires with common js requires themselves
+    // global requires are assumed to be requirejs requires
     source = source.replace(cjsRequireRegExp, function (match, dep) {
-      return ' require(\'cjs!' + dep + '\')';
+      if (dep.substr(0, 1) == '.')
+        return ' require(\'cjs!' + dep + '\')';
+      else
+        return ' require(\'' + dep + '\')';
     });
     // wrap up in common js wrapper
     callback('define(function(require, exports, module) { (function(){var define=undefined;' + source + ' \n})() });');
